5c5703b61eb5c7ea9ef4e22281008a12f6a07f69
Md. Golam Rabby Jim <jim15-5551@diu.bd>
2020-05-06 10:15:25 +0600

    get_password_leaks_counts() method added

--- Password Checker/venv/mian.py
+++ Password Checker/venv/mian.py
@@ -9,10 +9,21 @@ def request_api_data(quary_chars):
         raise RuntimeError(f"Error Fetching: {response.status_code}, Check your url and try again!!!")
     return response
 
+def get_password_leaks_counts(hashes, hash_to_check):
+    hashes = (line.split(':') for line in hashes.text.splitlines())
+    for h, count in hashes:
+        print(h, count)
+
+
+
 
 def pwned_api_checker(password):
     sha1_password = hashlib.sha1(password.encode("utf-8")).hexdigest().upper()
-    return sha1_password
+    first_5_char , tail = sha1_password[:5] , sha1_password[5:]
+    print(first_5_char, tail)
+    responses =  request_api_data(first_5_char)
+    return get_password_leaks_counts(responses, tail)
+
 
 print(pwned_api_checker("40408080"))
 
